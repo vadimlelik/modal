8f9fc96ebbe429b9cfcb029c4391b68ceee16271
vadimcvirko <promiti90@gmail.com>
2023-03-04 08:50:54 +0300

    finish project

--- src/component/Contenr/Content.jsx
+++ src/component/Contenr/Content.jsx
@@ -1,31 +1,37 @@
 import React, { useState } from "react";
+import { createPortal } from 'react-dom';
 import style from "./Content.module.css";
 import cn from "classnames";
 import Button from "../Button/Button";
 import Modal from "../Modal/Modal";
 
+
 const Content = (props) => {
   const { className, ...other } = props;
-  const [open, setIsOpen] = useState(true);
+  const [open, setIsOpen] = useState(false);
   return (
     <div className={cn(style.Content, {}, [className])} {...other}>
+
       {/* <Button appearance="orange" arrow>
         Купить
       </Button>
       <Button appearance="danger">Удалить</Button> */}
-      <Button appearance="orange"> Open Modal</Button>
 
-      <Modal
-        isOpen={open}
-        onClose={() => {
-          setIsOpen(false);
-        }}
-      >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eligendi
-        autem, pariatur neque cum maiores animi. A assumenda veniam quaerat,
-        sint, sapiente laborum quisquam recusandae consequatur pariatur nam modi
-        quae inventore facilis?
-      </Modal>
+      <Button appearance="orange" onClick={()=>setIsOpen(true)} > Open Modal</Button>
+
+            <Modal
+            isOpen={open}
+            onClose={() => {
+                setIsOpen(false);
+            }}
+        >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Non eligendi
+            autem, pariatur neque cum maiores animi. A assumenda veniam quaerat,
+            sint, sapiente laborum quisquam recusandae consequatur pariatur nam modi
+            quae inventore facilis?
+        </Modal>
+
+
     </div>
   );
 };
--- src/component/Modal/Modal.jsx
+++ src/component/Modal/Modal.jsx
@@ -1,25 +1,68 @@
-import React, { useCallback, useState } from "react";
+import React, {useCallback, useEffect, useRef, useState} from "react";
 import style from "./Modal.module.css";
 import cn from "classnames";
+import Portal from "../Portal/Portal.jsx";
+
+
+const timer = null
 
 const Modal = (props) => {
-  const { className, children, isOpen, onClose } = props;
+    const {className, children, isOpen, onClose} = props;
 
-  //   const [isClosing, setIsClosing] = useState(false);
+    // закрытие модалки с анимацией,
+    const [isClosing, setIsClosing] = useState(false);
 
-  const closeHandler = () => {
-    onClose();
-  };
+    const timeRef = useRef()
 
-  return (
-    <div className={cn(style.Modal, { [style.opened]: isOpen }, [className])}>
-      <div className={style.overlay} onClick={closeHandler}>
-        <div className={style.content} onClick={(e) => e.stopPropagation()}>
-          {children}
-        </div>
-      </div>
-    </div>
-  );
+    const closeHandler = useCallback(() => {
+        if (onClose) {
+
+            setIsClosing(true)
+
+            timeRef.current = setTimeout(() => {
+                onClose()
+                setIsClosing(false)
+            }, 300)
+        }
+
+    }, [onClose]);
+
+
+    const onKeyDown = useCallback((e) => {
+        if (e.key === 'Escape') {
+            closeHandler()
+        }
+    }, [isOpen])
+
+
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown)
+        }
+
+
+        return () => {
+            clearTimeout(timeRef.current)
+            window.removeEventListener('keydown', onKeyDown)
+
+        }
+    }, [isOpen, onKeyDown])
+
+
+    return (
+        <Portal>
+            <div className={cn(style.Modal, {
+                [style.opened]: isOpen,
+                [style.isClosing]: isClosing
+            }, [className])}>
+                <div className={style.overlay} onClick={closeHandler}>
+                    <div className={style.content} onClick={(e) => e.stopPropagation()}>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </Portal>
+    );
 };
 
 export default Modal;
